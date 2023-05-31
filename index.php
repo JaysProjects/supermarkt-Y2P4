a04a632950a2b28c2d5fdd0ae5aca341a8f86f0c
JXYZP <jahden@live.nl>
2023-05-31 19:33:33 +0200

    Add index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include_once "class/db_config.php";
+include_once "class/DatabaseConnection.php";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Client Form</title>
+</head>
+<body>
+    <a href="forms/add_client.php">Add client</a>
+    <br>
+    <a href="forms/add_sale.php">Add Sale</a>
+    <br>
+    <a href="selections/sales.php">Show Orders</a>
+</body>
+</html>
