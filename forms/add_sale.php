ee6be335b3180e76a805c716590642134e984030
JXYZP <jahden@live.nl>
2023-06-07 15:13:37 +0200

    Add drop down functionallity while creating a new order

--- forms/add_sale.php
+++ forms/add_sale.php
@@ -2,8 +2,13 @@
 include_once "../class/db_config.php";
 include_once "../class/DatabaseConnection.php";
 include_once "../class/SalesHandler.php";
+include_once "../class/ClientHandler.php";
+include_once "../class/ProductHandler.php";
 
 $salesFormHandler = new SalesHandler($database->getConnection());
+$klanten = new ClientHandler($database->getConnection());
+$productHandler = new ProductHandler($database->getConnection());
+
 $salesFormHandler->handleFormSubmission();
 
 $database->closeConnection();
@@ -19,10 +24,25 @@ $database->closeConnection();
     <h2>New Sales Form</h2>
     <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <label for="klantId">Klanten_klantId:</label>
-        <input type="text" id="klantId" name="klantId" required><br><br>
+        <select name="klantId" id="klantId" required>
+            <option value="" disabled selected>Choose here</option>
+            <?php
+            $klantenList = $klanten->getKlanten();
+            foreach ($klantenList as $klant) {
+                echo '<option value="' . $klant['klantId'] . '">' . $klant['klantId'] . ' - ' . $klant['klantNaam'] . '</option>';        }
+            ?>
+        </select><br><br>
 
         <label for="artId">Artikelen_artId:</label>
-        <input type="text" id="artId" name="artId" required><br><br>
+        <select name="artId" id="artId" required>
+            <option value="" disabled selected>Choose here</option>
+            <?php
+            $productList = $productHandler->getProducts();
+            foreach ($productList as $product) {
+                echo '<option value="' . $product['artId'] . '">' . $product['artId'] . '</option>';
+            }
+            ?>
+        </select><br><br>
 
         <label for="verOrdBestAantal">verOrdBestAantal:</label>
         <input type="text" id="verOrdBestAantal" name="verOrdBestAantal" required><br><br>
